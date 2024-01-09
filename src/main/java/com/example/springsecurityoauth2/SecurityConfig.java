6d406a3c981a3dcf0a359a73381d932003295d4c
eod940 <eod940@gmail.com>
2024-01-09 19:50:39 +0900

    자동 설정에 의한 초기화 과정 이해(2)

--- src/main/java/com/example/springsecurityoauth2/SecurityConfig.java
+++ src/main/java/com/example/springsecurityoauth2/SecurityConfig.java
@@ -8,14 +8,25 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
 
+  // 싱글톤 아니니까 2개가 생성되는걸 확인
   @Bean
-  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+  public SecurityFilterChain securityFilterChain1(HttpSecurity http) throws Exception {
     http.authorizeRequests(request -> request
             .anyRequest()
             .authenticated())
-        .formLogin()
-        .and()
-        .apply(new CustomSecurityConfigurer().setFlag(false));
+        .formLogin();
+//        .apply(new CustomSecurityConfigurer().setFlag(false));
+
+    return http.build();
+  }
+
+  @Bean
+  public SecurityFilterChain securityFilterChain2(HttpSecurity http) throws Exception {
+    http.authorizeRequests(request -> request
+            .anyRequest()
+            .authenticated())
+//        .formLogin()
+        .httpBasic();
 
     return http.build();
   }
